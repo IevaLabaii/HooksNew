701b7328959aa5ed14d55e039748c0b05bc1a08b
Ieva <i.labalaukyte@gmail.com>
2020-03-01 21:44:11 +0200

    First commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//import App from './App';
+
+const App = () => {
+
+const [currentInput, setCurrentInput] = React.useState('')
+const [list, setList] = React.useState([]);
+
+const AddTodo = (event) => {
+  event.preventDefault();
+  const newList = [...list];
+  newList.push(currentInput);
+  setList([...newList]);
+}
+
+  return (
+    <div>
+      <div className="outer-box">
+          <h1>To do:</h1>
+          <form className="input-box">
+            <input className="input" type="text" placeholder="Enter text" onChange={(event)=>{setCurrentInput(event.target.value)}}/>
+            <button className="add-button" onClick={AddTodo}>Add</button>
+          </form>
+          {list.map((singleTodo)=>{
+            return <div>{singleTodo}</div>
+          })}
+      </div>
+    </div>
+  )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
